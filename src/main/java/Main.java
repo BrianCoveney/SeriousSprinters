b1a8368952676edf2e5e37dfbecb9d8a5b91a568
BrianCoveney <coveneybrian@gmail.com>
2018-04-23 12:39:37 +0100

    Add not on CheckStyle rules

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import static java.lang.System.out;
 
 public class Main {
 
+    /*
+    * 'CHECKSTYLE IGNORE' is not coding documentation comments,
+    * they're part of our CheckStyle rules.
+    */
+
     // CHECKSTYLE IGNORE check FOR NEXT 1 LINES
     public static void main(String[] args) {
         // CHECKSTYLE IGNORE check FOR NEXT 1 LINES
