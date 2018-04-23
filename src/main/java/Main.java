45708c87da3acd0d5b7a24c7b05698dacd183951
BrianCoveney <coveneybrian@gmail.com>
2018-04-23 13:26:54 +0100

    Add comments in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import static java.lang.System.out;
 
 public class Main {
 
-
+    /*
+    * 'CHECKSTYLE IGNORE' is not coding documentation comments,
+    * they're part of our CheckStyle rules.
+    */
 
     // CHECKSTYLE IGNORE check FOR NEXT 1 LINES
     public static void main(String[] args) {
